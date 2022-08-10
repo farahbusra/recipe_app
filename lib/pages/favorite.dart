584a691afe652610a950685b7b2bfa6cf4921927
Farah Afiqah <farah.afiqah@besquare.com.my>
2022-08-10 12:15:26 +0800

    fav page

--- lib/pages/favorite.dart
+++ lib/pages/favorite.dart
@@ -2,6 +2,116 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:recipe_app/pages/index.dart';
 
+class recipeCard extends StatelessWidget {
+  final String title;
+  final String rating;
+  final String cookTime;
+  final String thumbnailUrl;
+  recipeCard({
+    @required this.title,
+    @required this.cookTime,
+    @required this.rating,
+    @required this.thumbnailUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6),
+            offset: Offset(
+              0.0,
+              10.0,
+            ),
+            blurRadius: 10.0,
+            spreadRadius: -6.0,
+          ),
+        ],
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.35),
+            BlendMode.multiply,
+          ),
+          image: NetworkImage(thumbnailUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 19,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            alignment: Alignment.center,
+          ),
+          Align(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      Text(rating),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.schedule,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      Text(cookTime),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            alignment: Alignment.bottomLeft,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class FavouritePage extends StatefulWidget {
   const FavouritePage({Key? key}) : super(key: key);
 
@@ -26,7 +136,7 @@ class _FavouritePageState extends State<FavouritePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Recipe',
+              'Favourite',
               style: TextStyle(color: paragraphColor),
             ),
             SizedBox(width: 6),
@@ -36,7 +146,13 @@ class _FavouritePageState extends State<FavouritePage> {
         centerTitle: true,
         backgroundColor: primaryColor,
       ),
-      body: Center(child: Text('this is the recipe page')),
+      body: recipeCard(
+        title: "My recipe",
+        reting: '4.9',
+        cookTime: '30 min',
+        thumbnailUrl:
+            'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+      ),
     );
   }
 }
