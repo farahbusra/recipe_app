b93417c89a95cfe085b029c0dbb225d882337375
Shawn Lim <lsy2pw@gmail.com>
2022-08-12 08:53:57 +0800

    commit before merge

--- lib/pages/recipe.dart
+++ lib/pages/recipe.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_app/models/recipe_model.dart';
+import 'package:recipe_app/pages/favorite.dart';
 import 'package:recipe_app/pages/recipe_detail.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recipe_app/pages/recipe_full_info.dart';
@@ -170,7 +171,7 @@ class _RecipePageState extends State<RecipePage> {
                 ),
               ),
               const SizedBox(
-                height: 12,
+                height: 8,
               ),
               Container(
                 height: 70,
@@ -195,10 +196,107 @@ class _RecipePageState extends State<RecipePage> {
                 ),
               ),
               const SizedBox(
-                height: 12,
+                height: 8,
               ),
               // Try to recall all the food list
-              RecipeList()
+              Container(
+                width: 280,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 30),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'chicken'));
+                        },
+                        child: recipeCard(
+                          title: "Chicken Dish",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/mUm3dii5LvNDoVJ6VeRhxj-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'pie'));
+                        },
+                        child: recipeCard(
+                          title: "Pies",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/ojyy8dW7aDupLbPGjMDgFA-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'cheese'));
+                        },
+                        child: recipeCard(
+                          title: "Cheezy",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/7hPCQ8T4PH8e7gC4q5GnF4-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'fish'));
+                        },
+                        child: recipeCard(
+                          title: "Fish Dish",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/9gqY3eeyvGFDdbwjSH7qkg-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'curry'));
+                        },
+                        child: recipeCard(
+                          title: "Curry",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/pM6x7NJ3TWWW5hS3Y8st8Z-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'lamb'));
+                        },
+                        child: recipeCard(
+                          title: "Lamb",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/qAoTS5bzvgYPuHENDtu2n4-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'egg'));
+                        },
+                        child: recipeCard(
+                          title: "Egg Dish",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/kHhBBQ9pmMEimcq5PL8SZF-768-80.jpg.webp',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/recipedetail',
+                              arguments: RecipeDetail(foodName: 'beef'));
+                        },
+                        child: recipeCard(
+                          title: "Something Beefy",
+                          thumbnailUrl:
+                              'https://cdn.mos.cms.futurecdn.net/kHhBBQ9pmMEimcq5PL8SZF-768-80.jpg.webp',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -267,9 +365,13 @@ class _RecipePageState extends State<RecipePage> {
 }
 
 class RecipeList extends StatelessWidget {
-  const RecipeList({required this.recipeName, Key? key}) : super(key: key);
+  late final String title;
 
-  final RecipeName recipeName;
+  late final String thumbnailUrl;
+  recipeCard({
+    required this.title,
+    required this.thumbnailUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -277,88 +379,61 @@ class RecipeList extends StatelessWidget {
     const paragraphColor = Color(0xFF2B2C34);
     const buttonTextColor = Color(0xFFFFFFFE);
     const buttonColor = Color(0xFF6246EA);
-    const secondaryColor = Color(0xFFD1D1E9);
     const tertiaryColor = Color(0xFFE45858);
 
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: recipeName.meals.length,
-      physics: ScrollPhysics(),
-      itemBuilder: ((context, index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, '/recipefullinfo',
-                arguments:
-                    RecipeFullInfo(meal: recipeName.meals.elementAt(index)));
-          },
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 16),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color:
-                      const Color.fromARGB(255, 192, 196, 255).withOpacity(0.4),
-                  width: 1,
+    return Container(
+      margin: EdgeInsets.only(
+        top: 0,
+        right: 20,
+        bottom: 30,
+        left: 20,
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6),
+            offset: Offset(
+              0.0,
+              10.0,
+            ),
+            blurRadius: 10.0,
+            spreadRadius: -6.0,
+          ),
+        ],
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.38),
+            BlendMode.multiply,
+          ),
+          image: NetworkImage(thumbnailUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: primaryColor,
+                  fontWeight: FontWeight.w600,
                 ),
-              ),
-              child: Row(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.network(
-                    '${recipeName.meals.elementAt(index).strMealThumb}',
-                    fit: BoxFit.fill,
-                    height: 180,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ConstrainedBox(
-                        constraints: const BoxConstraints(
-                          maxWidth: 200,
-                        ),
-                        child: //move the contaier here
-                            Container(
-                          width: 180,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '${recipeName.meals.elementAt(index).strMeal}',
-                                  style: GoogleFonts.lato(
-                                    textStyle: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w900,
-                                      color: paragraphColor,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 12),
-                                Text(
-                                  'Origin: ${recipeName.meals.elementAt(index).strArea}',
-                                  style: GoogleFonts.lato(
-                                    textStyle: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                      color: paragraphColor,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textAlign: TextAlign.center,
               ),
             ),
+            alignment: Alignment.center,
           ),
-        );
-      }),
+        ],
+      ),
     );
   }
 }
